a9fc850919c7272a051ff18a62936d6e5a92b880
mostafataher-1911 <mostafataher339@gmail.com>
2025-10-27 19:05:36 +0200

    add vercal.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -14,4 +13,8 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist', // مهم علشان Vercel يلاقي الملفات
+  },
+  base: '/', // علشان المسارات تشتغل كويس بعد الرفع
 })
